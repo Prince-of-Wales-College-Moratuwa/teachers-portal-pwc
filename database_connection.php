a2047e77dd515172b30052a33e02e6bd1ccab9b8
nelitha13 <nelithavindinu7@gmail.com>
2024-06-17 16:28:05 +0530

    h

--- database_connection.php
+++ database_connection.php
@@ -8,6 +8,3 @@ $connect = new PDO("mysql:host=localhost; dbname=teacher_pwc_db", "root", "");
 
 // $connect = new PDO("mysql:host=localhost:3306; dbname=teacher_pwc_db", "admin_pwc", "s63pv7I@1");
 // $connect->exec("set names utf8mb4");
-
-
-?>
